abf08ff2b282e12fdd83442d356a7238cb1e57f7
Franck <136079691+Freddyede@users.noreply.github.com>
2024-05-02 16:33:24 +0200

    ignore dist directory

--- dist/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Core_1 = require("./core/Core");
-new Core_1.Core();
-//# sourceMappingURL=app.js.map
--- dist/tests/app.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Core_test_1 = require("./Core.test");
-const coreTest = new Core_test_1.CoreTest();
-coreTest.testDateIsAnObject();
-coreTest.testDateYearsIsGoodYear();
-//# sourceMappingURL=app.test.js.map
